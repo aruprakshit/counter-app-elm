8ac64b39377967efa666296f71008976fe3a5657
Arup Rakshit <aruprakshit@rocketmail.com>
2018-11-12 13:25:58 +0530

    first port program

---
+++ src/Main.elm
@@ -0,0 +1,52 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+type alias Model =
+    { count : Int }
+
+
+initialModel : Int -> ( Model, Cmd msg )
+initialModel flags =
+    ( Model flags, Cmd.none )
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( { model | count = model.count + 1 }, Cmd.none )
+
+        Decrement ->
+            ( { model | count = model.count - 1 }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Increment ] [ text "+1" ]
+        , div [] [ text <| String.fromInt model.count ]
+        , button [ onClick Decrement ] [ text "-1" ]
+        ]
+
+
+subscriptions model =
+    Sub.none
+
+
+main : Program Int Model Msg
+main =
+    Browser.element
+        { init = initialModel
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
