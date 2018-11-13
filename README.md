dcfb7eb257fab3624b95f1ee4d10c313a160ca09
Arup Rakshit <aruprakshit@rocketmail.com>
2018-11-14 01:05:49 +0530

    Add incoming, outgoing port

---
+++ README.md
@@ -0,0 +1,2 @@
+# Elm port examples
+https://gist.github.com/azer/afefbb0d9d9c4c30249718e87aa8acd0
--- src/Main.elm
+++ src/Main.elm
@@ -1,50 +1,143 @@
-module Main exposing (main)
+port module Main exposing (Model, Msg(..), initialModel, main, subscriptions, update, view)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import Json.Decode as D
+import Json.Encode as E
 
 
 type alias Model =
-    { count : Int }
+    { count : Int
+    , incBy : Int
+    , decBy : Int
+    }
+
+
+type alias Scale =
+    { x : Int
+    , y : Int
+    }
 
 
 initialModel : Maybe Int -> ( Model, Cmd msg )
 initialModel flags =
     case flags of
         Just flagVal ->
-            ( Model flagVal, Cmd.none )
+            ( Model flagVal 1 1, Cmd.none )
 
         Nothing ->
-            ( Model 0, Cmd.none )
+            ( Model 0 1 1, Cmd.none )
 
 
 type Msg
     = Increment
     | Decrement
+    | Received E.Value
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            ( { model | count = model.count + 1 }, Cmd.none )
+            let
+                newCount =
+                    model.count + model.incBy
+            in
+            ( { model | count = newCount }, toJs <| E.int newCount )
 
         Decrement ->
-            ( { model | count = model.count - 1 }, Cmd.none )
+            let
+                newCount =
+                    model.count - model.decBy
+            in
+            ( { model | count = newCount }, toJs <| E.int newCount )
+
+        Received val ->
+            let
+                record_ =
+                    decodeCounterScale val
+            in
+            ( { model | incBy = record_.x, decBy = record_.y }, Cmd.none )
+
+
+
+-- View
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
+    div
+        [ style "display" "flex"
+        , style "width" "300px"
+        , style "justify-content" "space-around"
+        , style "background-color" "#ddd"
+        , style "padding" "4em"
+        , style "margin" "auto"
+        , style "font-size" "20px"
+        ]
+        [ button
+            [ onClick Increment
+            , style "color" "#FFFFFF"
+            , style "background-color" "#4CAF50"
+            , style "font-weight" "800"
+            , style "font-size" "1em"
+            ]
+            [ text ("+" ++ String.fromInt model.incBy) ]
+        , div
+            [ style "background-color" "#90caf9"
+            , style "padding" "20px"
+            , style "font-weight" "800"
+            , style "color" "red"
+            , style "font-size" "2em"
+            ]
+            [ text <| String.fromInt model.count ]
+        , button
+            [ onClick Decrement
+            , style "color" "#FFFFFF"
+            , style "background-color" "#4CAF50"
+            , style "font-weight" "800"
+            , style "font-size" "1em"
+            ]
+            [ text ("-" ++ String.fromInt model.decBy) ]
         ]
 
 
+subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    toElm Received
+
+
+counterScaleDecoder : D.Decoder Scale
+counterScaleDecoder =
+    D.map2 Scale
+        (D.field "x" D.int)
+        (D.field "y" D.int)
+
+
+
+-- Decoders
+
+
+decodeCounterScale : D.Value -> Scale
+decodeCounterScale value =
+    case D.decodeValue counterScaleDecoder value of
+        Ok scales ->
+            scales
+
+        Err _ ->
+            Scale 1 1
+
+
+
+-- Ports
+
+
+port toJs : E.Value -> Cmd msg
+
+
+port toElm : (E.Value -> msg) -> Sub msg
 
 
 main : Program (Maybe Int) Model Msg
